f8103f2691719f862b5b02ef6a50166c608a7430
ch672 <45762341+ch672@users.noreply.github.com>
2019-01-16 15:50:11 +0000

    Update to login.js
    Update to get 'showPassword' to work

--- public/js/login.js
+++ public/js/login.js
@@ -18,18 +18,23 @@ studentRadio.addEventListener("RadioStateChange", onRadioStateChange);
 
 
 
+    //Set default value for if password is clicked
+    var passwordClicked = false;
+
     function showHidePass() {
-        var getLoginInput = document.getElementById("passwordClicked");
+    var getLoginInput = document.getElementById("passwordClicked");
 
-        if (checkbox.checked) {
-            //If button is clicked, change to 'text' type to see what password is
-            getLoginInput.setAttribute("type", "text");
+    if(passwordClicked === false) {
+    //If button is clicked, change to 'text' type to see what password is
+    getLoginInput.setAttribute("type", "text");
+    passwordClicked = true;
 
-        } else if (!checkbox.checked) {
-            //If button is clicked again, change from 'text' type to 'password'
-            getLoginInput.setAttribute("type", "password");
-        }
-    }
+  } else if (passwordClicked === true) {
+    //If button is clicked again, change from 'text' type to 'password'
+    getLoginInput.setAttribute("type", "password");
+    passwordClicked = false;
+  }
+}
 
 
 
