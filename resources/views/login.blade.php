5c3aef3725d3002f4627b4890ca412aa9d70fcb1
ryan <ryand9508@hotmail.co.uk>
2019-01-18 15:05:09 +0000

    reg link

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -45,7 +45,7 @@
 
 
     <br><br>
-    <a id="registerLink" href="Registration.html">Don't have an account? Click here</a><br>
+    <a id="registerLink" href="reg">Don't have an account? Click here</a><br>
     <a id="forgotPassword" href="/meme.html">Forgot your password? Click here</a>
 </div>
 </body>
