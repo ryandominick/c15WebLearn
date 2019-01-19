87460f1cbfaad72fbb5ba695d4d08e9e87581a0d
ryan <ryand9508@hotmail.co.uk>
2019-01-19 13:51:47 +0000

    createQuiz progress

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -3,16 +3,38 @@
 namespace App\Models;
 
 
+
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Quiz extends Model
+{
+
+    public static function addQuestionMC($mcQuestion, $correctAns, $altAns1, $altAns2, $altAns3)
+    {
+
+        DB::insert('insert into mcquestion(mcQuestion, correctAns, altAns1, altAns2, altAns3) values (:mcQuestion, :correctAns, :altAns1, :altAns2, :altAns3)',
+
+            [':mcQuestion' => $mcQuestion, ':correctAns' => $correctAns, ':altAns1' => $altAns1, ':altAns2' => $altAns2, ':altAns3' => $altAns3]);
 
 
-class Quiz extends Model{
+    }
 
-    public static function addQuiz(){
+    public static function addQuestionInput($inputQuestion, $inputAnswer)
+    {
+            DB::insert('insert into inputquestion(inputQuestion, inputAnswer) values (:inputQuestion, :inputAnwer)',
+                [':inputQuestion' => $inputQuestion, ':inputAnswer' => $inputAnswer]);
 
 
+    }
 
+    public static function findQuizID(){
 
+        $maxID = DB::select('select max(quizID) from teacherquiz');
+
+        $tempMaxID = get_object_vars($maxID[0]);
+
+        return $tempMaxID;
     }
 
 }
