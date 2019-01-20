3ce09759ef5470f23a7705e2ba1e0c6b9ad1d269
hb415 <hb415@kent.ac.uk>
2019-01-20 15:39:52 +0000

    fixed login js dependency issue

--- public/js/login.js
+++ public/js/login.js
@@ -6,47 +6,50 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-let checkbox = document.getElementById("checkbox1");
-let loginForm = document.getElementById("loginForm");
-let studentRadio = document.getElementById("stuRadio");
-let teacherRadio = document.getElementById("teaRadio");
-
-checkbox.addEventListener("CheckboxStateChange", showHidePass);
-teacherRadio.addEventListener("RadioStateChange", onRadioStateChange);
-studentRadio.addEventListener("RadioStateChange", onRadioStateChange);
-
+    let checkbox = document.getElementById("checkbox1");
+    let loginForm = document.getElementById("loginForm");
+    let studentRadio = document.getElementById("stuRadio");
+    let teacherRadio = document.getElementById("teaRadio");
 
 
     //Set default value for if password is clicked
-    var passwordClicked = false;
+    let passwordClicked = false;
+
+    //checkbox.click();
 
     function showHidePass() {
-    var getLoginInput = document.getElementById("passwordClicked");
+        let getLoginInput = document.getElementById("passwordClicked");
 
-    if(passwordClicked === false) {
-    //If button is clicked, change to 'text' type to see what password is
-    getLoginInput.setAttribute("type", "text");
-    passwordClicked = true;
+        if(passwordClicked === false) {
+            //If button is clicked, change to 'text' type to see what password is
+            getLoginInput.setAttribute("type", "text");
+            passwordClicked = true;
 
-  } else if (passwordClicked === true) {
-    //If button is clicked again, change from 'text' type to 'password'
-    getLoginInput.setAttribute("type", "password");
-    passwordClicked = false;
-  }
-}
-
-
-
-function onRadioStateChange(event) {
-    let radio = event.target;
-    if (radio.checked && radio.value === "student") {
-        loginForm.action = "/student/login"
-        //alert(loginForm.action);
-
-    } else if (radio.checked && radio.value === "teacher") {
-        loginForm.action = "/teacher/login"
-        //alert(loginForm.action);
+        } else if (passwordClicked === true) {
+            //If button is clicked again, change from 'text' type to 'password'
+            getLoginInput.setAttribute("type", "password");
+            passwordClicked = false;
+        }
     }
-}
+
+
+
+    function onRadioStateChange(event) {
+        let radio = event.target;
+        if (radio.checked && radio.value === "student") {
+            loginForm.action = "/student/login"
+            //alert(loginForm.action);
+
+        } else if (radio.checked && radio.value === "teacher") {
+            loginForm.action = "/teacher/login"
+            //alert(loginForm.action);
+        }
+    }
+
+    checkbox.addEventListener("click", showHidePass);
+    teacherRadio.addEventListener("click", onRadioStateChange);
+    studentRadio.addEventListener("click", onRadioStateChange);
+
+
 
 });
