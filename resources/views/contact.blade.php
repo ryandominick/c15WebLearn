e039c3d6280f3c87a36a72654e10790e221bc5e0
ryan <ryand9508@hotmail.co.uk>
2019-01-26 12:12:32 +0000

    fixed links on html and removed unused assets

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,28 +2,25 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Take Quiz</title>
+    <title>Contact Us</title>
     <link rel="stylesheet" type="text/css" href="/css/projstyle.css">
-    <script type="text/javascript" src="/js/takeQuiz.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 </head>
 <body>
 <div class="smallLogo">
     <img src="/images/GradiQuiz_Logo.png" style="height: 90px;" alt="logo"/>
 </div>
+<hr>
 <nav class="navigation">
-    <a id="home" href="/teacher/home">Home</a>
+    <a id="home" href="/student/home">Home</a>
     <a id="results" href="MyResults.html">My results</a>
     <a id="studProfile" href="StudentProfile.html">My Profile</a>
-    <a id="contact" href="/contact">Contact Us</a>
-    <a id="logout" href="/html/">Log out</a>
+    <a id="logout" href="/student/logout">Log out</a>
 </nav>
-<h2 id="quizHeader">Quiz</h2>
-<form id ="takeQuizForm" action="submit.php">
-</form>
-<div class="QuizStyle">
-    <h2>If you are happy with your answers, submit now</h2>
-    <input type="submit" id="quizSubmit" value="Submit Quiz Now">
-</div>
+<hr>
+<h2 id="contactUs">Contact Us</h2>
+<p id="techInfo">For technical support, bug reports and recommendations, please contact us here: <br>
+    <a href="mailto:GradiQuiz@gmail.com">GradiQuiz@gmail.com</a>
+</p>
 </body>
 </html>
--- resources/views/studentHomepage.blade.php
+++ resources/views/studentHomepage.blade.php
@@ -16,7 +16,7 @@
     <a id="home" href="/student/home">Home</a>
     <a id="studProfile" href="/StudentProfile.html">My Profile</a>
     <a id="results" href="MyResults.html">My results</a>
-    <a class="floatRight" href="contact.html">Contact Us</a>
+    <a class="floatRight" href="/contact">Contact Us</a>
     <a class="floatRight" href="/student/logout">Logout</a>
 </nav>
 <hr>
