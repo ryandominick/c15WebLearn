ad60233419c86bad63a98c7896cfdb01c3e61e1c
cole <wowcole@hotmail.co.uk>
2019-01-27 17:58:10 +0000

    Create Quiz page 'refactored'

--- resources/views/createQuiz.blade.php
+++ resources/views/createQuiz.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <title>Create Quiz</title>
@@ -14,7 +13,7 @@
     <img src="/images/GradiQuiz_Logo.png" style="height: 90px;" alt="logo" />
 </div>
 <nav class="navigation">
-    <a id="home" href="TeacherHomepage.html">Home</a>
+    <a id="home" href="/teacher/home">Home</a>
     <a id="contact" href="/contact">Contact Us</a>
     <a id="logout" href="/teacher/logout">Logout</a>
 </nav>
@@ -23,57 +22,47 @@
 <form method="post" action="/createquiz" id="quizInfo">
     @csrf
     <div class="createContainer">
-
-
         <label class="quizDetails">
             <h3>Module Code:</h3>
         </label>
         <input class="createQuizDetails" id="quizModulePosition" type="text" placeholder="(e.g. CO434)" size="10" name="moduleCode"
-               required />
+               required/>
         <br>
         <label class="quizDetails">
             <h3>Quiz Title:</h3>
         </label>
         <input class="createQuizDetails" id="quizTitlePosition" type="text" placeholder="Please enter the quiz title (e.g. Database Systems)"
-               name="quizTitle" size="50" required />
-
+               name="quizTitle" size="50" required/>
         <br>
         <label class="quizDate">
             <h3>Start Date:</h3>
         </label>
-        <input id="quizStartDatePosition" type="date" name="quizDateStart" required />
-
+        <input id="quizStartDatePosition" type="date" name="quizDateStart" required/> <br>
         <label class="quizDate">
             <h3 id="endDateCaption">End Date:</h3>
         </label>
-        <input id="quizEndDatePosition" type="date" name="quizDateEnd" required />
+        <input id="quizEndDatePosition" type="date" name="quizDateEnd" required/>
         <br>
         <label class="quizDetails">
             <h3>Timer (minutes):</h3>
         </label>
-
         <label class="createQuizDetails">
-            <input id="quizTimerInput" type="number" placeholder="Please enter duration" name="timer" required />
-            <span id='timerValidityTxt'> </span>
+            <input id="quizTimerInput" type="number" placeholder="Please enter duration" name="timer" required/>
+            <span id='timerValidityTxt'></span>
         </label>
     </div>
     <div class = "questionContainer" id="questionSection">
-
-
     </div>
     {{--<button id="submitQuizButton" typ   e="submit" onclick="return submitAll()" value="Submit">Submit Quiz!</button>--}}
 </form>
-
-{{--<form method="post" id="createQuizForm" action="/createquiz">--}}
-    {{--@csrf--}}
-
-{{--</form>--}}
-
+{{--
+<form method="post" id="createQuizForm" action="/createquiz">--}}
+{{--@csrf--}}
+{{--
+</form>
+--}}
 <button class="addQuestions" id="addMCQuestions">Add Multiple Choice Question</button>
 <button class="addQuestions" id="addInputQuestions">Add Input Question</button>
-
 <button id="submitQuizButton" type="submit" onclick="return submitAll()" value="Submit">Submit Quiz!</button>
-
 </body>
-
 </html>
