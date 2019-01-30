22583688d28691d730d79e88f841dd2adfdfdb3a
cole <wowcole@hotmail.co.uk>
2019-01-30 22:21:11 +0000

    Conversation Model

---
+++ app/Models/Message.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use App\Models\Student;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    //
+
+
+
+    protected $fillable = ['body'];
+
+}
+
