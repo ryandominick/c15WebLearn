0809ef42cb359c006137b6804d8de57df7276a39
Ben <ben.danielgs@gmail.com>
2019-02-03 17:16:20 +0000

    created teacherSearch js file just updating master before I use new branch

--- app/Http/Controllers/StudentSearchController.php
+++ app/Http/Controllers/StudentSearchController.php
@@ -7,6 +7,18 @@ use Illuminate\Http\Request;
 
 class StudentSearchController extends Controller
 {
+
+    /**
+     *Create a new controller
+     *
+     * @return void
+     */
+
+    public function __construct()
+    {
+        $this->middleware('auth:student');
+    }
+
     public function index(){
 
         return view('studentSearch');
