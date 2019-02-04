7d8595da777c9debd833af7a91a2e13c85eed4f2
hb415 <hb415@kent.ac.uk>
2019-02-04 17:10:15 +0000

    fixed merge issue

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -35,41 +35,59 @@
         </div>
 
         <br><br>
-        <label for="email">
-            <input type ="email" placeholder = "University Email Address" size="35" name="email" required >
-        </label>
-        <br>
+        <div id = "registerIcons">
+            <i class ="fa fa-user icon"></i>
+            <label for="email">
+                <input type ="email" placeholder = "University Email Address" size="35" name="email" required >
+            </label>
+        </div>
 
+        <div id ="registerIcons">
+            <i id="passKeyIcon" class ="fa fa-key icon"></i>
         <label for="password">
             <input id="password" type ="password" placeholder = "Password" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" size="35"  required>
         </label>
         <div id = "eyeIconContainer">
-            <i class="fa fa-eye" id="eyeIcon"></i>
+            <i class="fa fa-eye-slash" id="eyeSlashIcon" aria-hidden="true" ></i>
+            {{--<i class="fa fa-eye" id="eyeIcon"></i>--}}
+
         </div>
         <div id="faPassword" class="fa">&#xf128;
             <span class="tooltipText">Password Strength Requirements: <strong>minimum 8 characters, at least 1 capital letter, lowercase letter and number.</strong></span>
         </div>
+        </div>
         <br>
 
+        <div id ="registerIcons">
+            <i id="conPassKeyIcon" class ="fa fa-key icon"></i>
         <label for="confirmPassword">
             <input id="confirmPassword" type ="password" placeholder = "Confirm Password" name="confirmPassword" size="35" onkeyup='checkPassword();' required>
+            <div id = "passwordValidityTxtContainer">
             <span id='passwordValidityTxt'> </span>
+            </div>
         </label>
-        <br>
+        </div>
 
+        <div id = "registerIcons">
+            <i class="fa fa-drivers-license-o icon"></i>
         <label for="firstName">
             <input type ="text" placeholder = "First Name" name="firstName" size="35" required>
         </label>
-        <br>
+        </div>
 
+        <div id = "registerIcons">
+            <i class="fa fa-drivers-license-o icon"></i>
         <label for="lastName">
             <input type ="text" placeholder = "Last Name" name="lastName" size="35" required>
         </label>
-        <br>
+        </div>
 
+        <div id = "registerIcons">
+            <i class="fa fa-graduation-cap icon" id="gradIcon"></i>
         <label for="courseName">
             <input id="courseInput" type ="text" placeholder = "Course Title (e.g. Computer Science)" name="courseName" size="35" required>
         </label>
+        </div>
         <br><br>
 
         <label for="lecturerKey">
