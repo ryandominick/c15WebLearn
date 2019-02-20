a7fcf7814967d3e0e238686271d7d375736ca163
Ben <ben.danielgs@gmail.com>
2019-02-20 13:50:28 +0000

    a test commit

--- public/js/registration.js
+++ public/js/registration.js
@@ -7,6 +7,9 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("eyeSlashIcon").addEventListener("click", showPassword);
     document.getElementById("stuRadio").addEventListener("click", disableInput);
     document.getElementById("teaRadio").addEventListener("click", disableInput);
+    document.getElementById("email").addEventListener("keyup", checkEmail); //Run checkEmail function when user presses a key in password
+
+    let $userType = "unassigned";
 
     function checkPassword() {
         if (document.getElementById('password').value !== "" && document.getElementById('confirmPassword').value !== "") {
@@ -34,6 +37,7 @@ document.addEventListener("DOMContentLoaded", function () {
             courseInput.style.backgroundColor = ''; //revert enabled field to default colour
             gradIcon.style.color = '';
             tooltipField.value = ""; //Remove lecture key value
+            $userType = "student"; //set the userType variable to student
         } else if (document.getElementById("teaRadio").checked === true) { //if teacher radio button is checked, enable lecture key field and disable course field
             document.getElementById("tooltipField").disabled = false;
             document.getElementById("courseInput").disabled = true;
@@ -41,6 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
             tooltipField.style.backgroundColor = ''; //revert enabled field to default colour
             gradIcon.style.color = '#A9A9A9'; //hide icon when selected
             courseInput.value = ""; //Remove course input value
+            $userType = "teacher"; //sets the userType to teacher
         } else {
             //do nothing
         }
@@ -63,5 +68,46 @@ document.addEventListener("DOMContentLoaded", function () {
             showPass.type = "password";
         }
     }
+
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+    function checkEmail(){
+
+        //check for students
+        if($userType === "student") {
+            let $email = $('#email').val();
+            if ($email.includes("@kent.ac.uk") ===true) {
+                $.ajax({
+                    url: '/reg/studentCheck',
+                    type: 'POST',
+                    data: $($email).serialize(),
+                    dataType: "int",
+
+                }).done(function($emailExists){
+
+                    if($emailExists ===  0){
+                        alert("does not exist");
+                    }
+                    else{
+
+                        alert("does exist");
+                    }
+
+                });
+
+
+            }
+        }
+
+        //check for teachers
+
+
+
+    }
+
 
 });
--- resources/views/createQuiz.blade.php
+++ resources/views/createQuiz.blade.php
@@ -8,8 +8,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script type="text/javascript" src="/js/navBar.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 </head>
 <body>
@@ -21,12 +20,12 @@
     <a href="/teacher/home"> <i class="fas fa-home" id="homeIcon"></i>Home</a>
 
     <div class="profileDropdown">
-        <button class="dropDownButton"><i class="fas fa-user-tie" id="userIcon"></i> <?php echo Auth::user()->firstName?>
+        <button class="dropDownButton"><i class="fas fa-user" id="userIcon"></i> <?php echo Auth::user()->firstName?>
             <i class="fa fa-caret-down" id="dropdwn"></i>
         </button>
-        <div class="dropdownLinksTeacher">
-            <a href="/teacher/profile"><i class="fas fa-user-tie" id="userIcon"></i>Profile</a>
-            <a href="/teacher/manageStudents"> <i class="fas fa-user-graduate" id="userIcon"></i>Manage Students</a>
+        <div class="dropdownLinks">
+            <a href="/teacher/profile">Profile</a>
+            <a href="/teacher/manageStudents">Manage Students</a>
             <a href="/teacher/logout"> <i class="fas fa-sign-out-alt" id="signOutIcon"></i>Logout</a>
         </div>
     </div>
@@ -34,71 +33,55 @@
 </div>
 
 <br>
-<br>
+<form method="post" action="/createquiz" id="quizInfo">
     @csrf
     <div class="createContainer">
-        <div id="centerQuizInfo">
-        <form method="post" action="/createquiz" id="quizInfo">
-        <div id ="registerIcons">
-            <i class ="fa fa-code icon"></i>
-        <input class="createQuizDetails" id="quizModulePosition" type="text" size="50" placeholder="Module Code"  name="moduleCode" required/>
-        </div>
-
+        <label class="quizDetails">
+            <h3>Module Code:</h3>
+        </label>
+        <input class="createQuizDetails" id="quizModulePosition" type="text" placeholder="(e.g. CO434)" size="10" name="moduleCode"
+               required/>
         <br>
-
-        <div id ="registerIcons">
-            <i class ="fa fa-pencil icon"></i>
-        <input class="createQuizDetails" id="quizTitlePosition" type="text" size="35" placeholder="Quiz Title" name="quizTitle" required/>
-        </div>
-
+        <label class="quizDetails">
+            <h3>Quiz Title:</h3>
+        </label>
+        <input class="createQuizDetails" id="quizTitlePosition" type="text" placeholder="(e.g. Database Systems)"
+               name="quizTitle" size="50" required/>
         <br>
-            <div id ="registerIcons">
-            <i id="calenPosition" class ="fa fa-calendar-check-o icon"></i>
-        <input id="quizStartDatePosition" type="date" name="quizDateStart" value="Start Date" required/> <br>
-            </div>
-            <br>
-            <div id ="registerIcons">
-            <i id="calenPosition" class ="fa fa-calendar-times-o icon"></i>
+        <label class="quizDate">
+            <h3>Start Date:</h3>
+        </label>
+        <input id="quizStartDatePosition" type="date" name="quizDateStart" required/> <br>
+        <label class="quizDate">
+            <h3 id="endDateCaption">End Date:</h3>
+        </label>
         <input id="quizEndDatePosition" type="date" name="quizDateEnd" required/>
-            </div>
-
         <br>
-
-        <div id ="registerIcons">
-            <i class ="fa fa-clock-o icon"></i>
-        <input id="quizTimerInput" type="number" placeholder="Quiz Duration (minutes)" name="timer" required/>
+        <label class="quizDetails">
+            <h3>Timer (minutes):</h3>
+        </label>
+        <label class="createQuizDetails">
+            <input id="quizTimerInput" type="number" placeholder="Duration of quiz" name="timer" required/>
             <span id='timerValidityTxt'></span>
-        </div>
-        </form>
-        </div>
+        </label>
     </div>
-
     <br>
-
-    <div class = "questionContainer" id="questionSection"></div>
-
+    <div class = "questionContainer" id="questionSection">
+    </div>
     <br>
-
+    {{--<button id="submitQuizButton" typ   e="submit" onclick="return submitAll()" value="Submit">Submit Quiz!</button>--}}
 </form>
-
 {{--
 <form method="post" id="createQuizForm" action="/createquiz">--}}
 {{--@csrf--}}
 {{--
 </form>
 --}}
-
 <div id="buttonWrapper">
-<div id ="createQuizButtons">
 <button class="addQuestions" id="addMCQuestions">Add Multiple Choice Question</button>
 <button class="addQuestions" id="addInputQuestions">Add Input Question</button>
-    <br>
-    <br>
-    <button id="submitQuizButton" type="submit" onclick="return submitAll()" value="Submit">Submit Quiz!</button>
 </div>
-</div>
-
-
-
+<br><br>
+<button id="submitQuizButton" type="submit" onclick="return submitAll()" value="Submit">Submit Quiz!</button>
 </body>
 </html>
