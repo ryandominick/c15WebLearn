0cd74496cba0a0f9b3c2ebd793874ae98632d719
Ben <ben.danielgs@gmail.com>
2019-02-20 14:06:02 +0000

    Bens removal of env file

--- public/js/navBar.js
+++
@@ -1,9 +0,0 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function burgerNav() {
-    var nav = document.getElementById("navBar");
-    if (nav.className === "navigation") {
-        nav.className += " responsive";
-    } else {
-        nav.className = "navigation";
-    }
-}
