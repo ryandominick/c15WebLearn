7ac1e77276e04d6e9475e19208d34906bb4ab99b
Ben <ben.danielgs@gmail.com>
2019-03-25 13:07:22 +0000

    moved deadlines query to TeacherQuiz model

--- app/Models/TeacherQuiz.php
+++ app/Models/TeacherQuiz.php
@@ -105,6 +105,18 @@ class TeacherQuiz extends Model
 
     }
 
+    public static function getQuizzesByStudentID($studentID)
+    {
+        return DB::select("
+          SELECT m.moduleCode, t.quizTitle, t.quizEnd, t.duration FROM Student AS s
+          INNER JOIN Course AS c ON c.courseID = s.courseID
+          INNER JOIN Module AS m ON m.courseID = c.courseID
+          INNER JOIN TeacherQuiz AS t ON t.moduleCode = m.moduleCode
+          WHERE s.id = :studentID  AND  (t.quizEnd BETWEEN NOW() AND DATE_ADD(NOW(),INTERVAL 7 DAY)) AND 
+          NOT EXISTS (SELECT * FROM Result AS r WHERE r.quizID = t.quizID AND 
+          r.studentID = :studentID1) LIMIT 20;  ",  ['studentID' => $studentID, 'studentID1' => $studentID]);
+    }
+
 
 }
 
